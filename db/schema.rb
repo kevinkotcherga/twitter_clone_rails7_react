554ef482978df9c00e03a81bece56208247eb676
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-02-10 10:59:27 +0100

    ✨feat(model): ajout du model tweet

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_02_095621) do
+ActiveRecord::Schema.define(version: 2022_02_10_095819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2022_02_02_095621) do
     t.index ["user_id"], name: "index_notes_on_user_id"
   end
 
+  create_table "tweets", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
@@ -32,4 +40,5 @@ ActiveRecord::Schema.define(version: 2022_02_02_095621) do
   end
 
   add_foreign_key "notes", "users"
+  add_foreign_key "tweets", "users"
 end
